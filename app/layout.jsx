c9e597c3adf42a8d27dba13623484eb8cee270c1
Waseem Mohammad <wmohammad83@icloud.com>
2024-09-05 22:16:17 +0100

    initial layout

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import "@/assets/styles/global.css";
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="text-2xl">HomePage</div>;
+};
+
+export default HomePage;
