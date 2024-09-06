beace0e125be00f9c9c5548af710526376e125a7
Waseem Mohammad <wmohammad83@icloud.com>
2024-09-06 19:14:48 +0100

    metadata and assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/global.css";
 
+export const metadata = {
+  title: "Property Palse",
+  keywords: "property, real estate, buy, sell, rent",
+  description: "Best property portal in the world",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
